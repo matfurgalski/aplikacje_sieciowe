7bb6bc7291530c02f246d8c06f0edd5747d652eb
Mateusz Furgalski <mateusz.furgalskii@gmail.com>
2022-10-21 20:43:10 +0200

    first commit

---
+++ php_01_widok_kontroler/app/calc_kredytowy.php
@@ -0,0 +1,76 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$x = $_REQUEST ['x'];
+$y = $_REQUEST ['y'];
+$oprocentowanie = $_REQUEST ['op'] ;
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($x) && isset($y) && isset($oprocentowanie))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $x == "") {
+	$messages [] = 'Nie podano kwoty';
+}
+if ( $y == "") {
+	$messages [] = 'Nie podano lat';
+}
+if ( $oprocentowanie == "") {
+	$messages [] = 'Nie podano oprocentowania';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+	if (! is_numeric( $x )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $oprocentowanie )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}	
+	if (! is_numeric( $y )) {
+		$messages [] = 'Trzecia wartość nie jest liczbą całkowitą';
+	}
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów na int
+	$x = intval($x);
+	$y = intval($y);
+	$oprocentowanie = floatval($oprocentowanie);
+	
+	
+	//wykonanie operacji
+	
+	$rata = $x / ($y * 12);
+
+	$opp = $rata * ($oprocentowanie / 100);
+
+	$result = $rata + $opp;
+
+	$result = round($result, 2);
+
+	}
+
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+
+include 'calc_kredytowy_view.php';
